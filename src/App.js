392da882e513da4f1e986f698a970bc799ec7d9f
BestebanTc <bestebantc@gmail.com>
2023-02-12 17:38:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,10 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HOLA MUNDO EN REACT XD <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link"  
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
