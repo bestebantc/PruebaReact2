b5e3fb98f80157f42d596b1638e1e9ca408ec624
BestebanTc <bestebantc@gmail.com>
2023-02-26 13:08:23 -0500

    Revert "A ver si asi se arregla alv"
    This reverts commit df13bce59b8682b71d7540720f5eaf8adc5e782b.

--- src/components/LoggedNavBar/index.js
+++ src/components/LoggedNavBar/index.js
@@ -14,7 +14,7 @@ export const LoggedNavBar = () => {
     return(
         <div className="navContainer">
             <div className="navTitle">
-                <NavLink to={'/home'} style={{color: 'white', textDecoration: 'none', fontSize: 58, fontFamily: 'fantasy'}}>MAKAI RANGER</NavLink>
+                <NavLink to={'/home'} style={{color: 'white', textDecoration: 'none', fontSize: 58, fontFamily: 'fantasy'}}>MAKAI RANGERR</NavLink>
                 <NavLink to={'/home'} style={{color: 'white', textDecoration: 'none', paddingLeft: '2%'}}>
                     <img src={require('../../assets/images/logo.png')} alt=""/>
                 </NavLink>
