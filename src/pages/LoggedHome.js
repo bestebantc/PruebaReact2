ba56daccd470a0388130fb0ec224a06f801894f9
BestebanTc <bestebantc@gmail.com>
2023-03-17 00:25:58 -0500

    se agregan dos graficas de prueba con react

--- src/pages/LoggedHome.js
+++ src/pages/LoggedHome.js
@@ -1,13 +1,70 @@
 import { LoggedNavBar } from "../components/LoggedNavBar";
 import '../styles/Logged.css'
 import { API, graphqlOperation } from "aws-amplify";
-import { useEffect, useState } from "react";
+import { useEffect, useState, React } from "react";
 import * as subscriptions from '../graphql/subscriptions';
 import {listTodos} from '../graphql/queries';
+import GaugeChart from 'react-gauge-chart'
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend
+  } from "recharts";
 
 const LoggedHome = () => {
+    const datosgraficaprueba = [
+        {
+          name: "Page A",
+          uv: 4000,
+          pv: 2400,
+          amt: 2400
+        },
+        {
+          name: "Page B",
+          uv: 3000,
+          pv: 1398,
+          amt: 2210
+        },
+        {
+          name: "Page C",
+          uv: 2000,
+          pv: 9800,
+          amt: 2290
+        },
+        {
+          name: "Page D",
+          uv: 2780,
+          pv: 3908,
+          amt: 2000
+        },
+        {
+          name: "Page E",
+          uv: 1890,
+          pv: 4800,
+          amt: 2181
+        },
+        {
+          name: "Page F",
+          uv: 2390,
+          pv: 3800,
+          amt: 2500
+        },
+        {
+          name: "Page G",
+          uv: 3490,
+          pv: 4300,
+          amt: 2100
+        }
+      ];
     const [data, setData] = useState([])
+    ////
 
+
+    /// 
     const status = {
         "status": ""
     }
@@ -55,7 +112,7 @@ const LoggedHome = () => {
     }, [data]);
 
     
-
+    console.log("distancia",data.distance)
     
     return(
 
@@ -65,6 +122,36 @@ const LoggedHome = () => {
                 <div className="loggedContainer2">
                     <div className="title">Bienvenido!</div>
                     <div className="loggedContent" >
+                    <LineChart
+                    width={500}
+                    height={300}
+                    data={datosgraficaprueba}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5
+                    }}
+                    >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line
+                        type="monotone"
+                        dataKey="pv"
+                        stroke="#8884d8"
+                        activeDot={{ r: 8 }}
+                    />
+                    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                    </LineChart>
+
+                    <GaugeChart id="gauge-chart2" 
+                        style={{"width": '30%'}}
+                        nrOfLevels={20} 
+                        percent={0.495} 
+                    />
                         PayLoadRecibida:
                         {data.map(function(item, i){
                         return <li key={i}>{item.distance} {item.status}</li>
