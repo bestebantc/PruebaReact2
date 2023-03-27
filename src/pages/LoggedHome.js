f98f4fdb9715e948fb2ae293a67f377ee5500228
BestebanTc <bestebantc@gmail.com>
2023-03-26 22:52:10 -0500

    Graficas Añadidas de Velocidad  Bateria y Alertas

--- src/pages/LoggedHome.js
+++ src/pages/LoggedHome.js
@@ -5,6 +5,13 @@ import { useEffect, useState, React } from "react";
 import * as subscriptions from '../graphql/subscriptions';
 import {listTodos} from '../graphql/queries';
 import GaugeChart from 'react-gauge-chart'
+import { color } from 'd3-color';
+import { interpolateRgb } from 'd3-interpolate';
+import ReactDOM from 'react-dom';
+import LiquidFillGauge from 'react-liquid-gauge';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+
 import {
     LineChart,
     Line,
@@ -15,7 +22,32 @@ import {
     Legend
   } from "recharts";
 
+  
 const LoggedHome = () => {
+  
+  const radius = 200;
+  const interpolate = interpolateRgb('#6495ed','#dc143c');
+  const fillColor = interpolate(50 / 100);
+  const gradientStops = [
+      {
+          key: '0%',
+          stopColor: color(fillColor).darker(0.5).toString(),
+          stopOpacity: 1,
+          offset: '0%'
+      },
+      {
+          key: '50%',
+          stopColor: fillColor,
+          stopOpacity: 0.75,
+          offset: '50%'
+      },
+      {
+          key: '100%',
+          stopColor: color(fillColor).brighter(0.5).toString(),
+          stopOpacity: 0.5,
+          offset: '100%'
+      }
+  ];
     const datosgraficaprueba = [
         {
           name: "Page A",
@@ -108,20 +140,17 @@ const LoggedHome = () => {
             console.log('terminating subscription to sensor');
             subscriber.unsubscribe();
         }
-    console.log("se termina suscripcion")
     }, [data]);
-
     
     console.log("distancia",data.distance)
-    
     return(
-
         <div className="page">
             <LoggedNavBar/>
             <div className="loggedContainer">
                 <div className="loggedContainer2">
                     <div className="title">Bienvenido!</div>
                     <div className="loggedContent" >
+
                     <LineChart
                     width={500}
                     height={300}
@@ -152,6 +181,75 @@ const LoggedHome = () => {
                         nrOfLevels={20} 
                         percent={0.495} 
                     />
+                    
+                    <LiquidFillGauge
+                    style={{ margin: '0 auto' }}
+                    width={radius * 2}
+                    height={radius * 2}
+                    value={50}
+                    percent="%"
+                    textSize={1}
+                    textOffsetX={0}
+                    textOffsetY={0}
+                    textRenderer={(props) => {
+                        const value = Math.round(props.value);
+                        const radius = Math.min(props.height / 2, props.width / 2);
+                        const textPixels = (props.textSize * radius / 2);
+                        const valueStyle = {
+                            fontSize: textPixels
+                        };
+                        const percentStyle = {
+                            fontSize: textPixels * 0.6
+                        };
+ 
+                        return (
+                            <tspan>
+                                <tspan className="value" style={valueStyle}>{value}</tspan>
+                                <tspan style={percentStyle}>{props.percent}</tspan>
+                            </tspan>
+                        );
+                    }}
+                    riseAnimation
+                    waveAnimation
+                    waveFrequency={2}
+                    waveAmplitude={1}
+                    gradient
+                    gradientStops={gradientStops}
+                    circleStyle={{
+                        fill: fillColor
+                    }}
+                    waveStyle={{
+                        fill: fillColor
+                    }}
+                    textStyle={{
+                        fill: color('#444').toString(),
+                        fontFamily: 'Arial'
+                    }}
+                    waveTextStyle={{
+                        fill: color('#fff').toString(),
+                        fontFamily: 'Arial'
+                    }}
+                />
+
+                    </div>
+                    <div className="loggedContent" >
+                    <Stack sx={{ width: '100%' }} spacing={2}>
+                    <Alert variant="filled" severity="error">
+                      This is an error alert — check it out!
+                    </Alert>
+                    <Alert variant="filled" severity="warning">
+                      This is a warning alert — check it out!
+                    </Alert>
+                    <Alert variant="filled" severity="info">
+                      This is an info alert — check it out!
+                    </Alert>
+                    <Alert variant="filled" severity="success">
+                      This is a success alert — check it out!
+                    </Alert>
+                  </Stack>
+                    </div>
+                    <div className="loggedContent" >
+                    
                         PayLoadRecibida:
                         {data.map(function(item, i){
                         return <li key={i}>{item.distance} {item.status}</li>
